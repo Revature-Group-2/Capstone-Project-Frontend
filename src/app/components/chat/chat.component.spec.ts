d2277ce9a3d8382e0eda7d3fa8bf9de585ebb0bf
HyungJoo Yoon <hyungjooyoon@disroot.org>
2022-12-13 14:21:51 -0800

    implemented some tests. added a service class in hopes it will work. please.

--- src/app/components/chat/chat.component.spec.ts
+++ src/app/components/chat/chat.component.spec.ts
@@ -1,14 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import * as SockJS from 'sockjs-client';
+import User from 'src/app/models/User';
+import { AuthService } from 'src/app/services/auth.service';
+import { over } from 'stompjs';
+
 
 import { ChatComponent } from './chat.component';
 
 describe('ChatComponent', () => {
   let component: ChatComponent;
   let fixture: ComponentFixture<ChatComponent>;
+  let httpSpy: jasmine.SpyObj<HttpClient>;
+  let authService: AuthService;
+  let user = new User(1,"email","first_name","last_name");
+  let authSpy: jasmine.SpyObj<AuthService>;
+  let wsSpy;
+  let clientSpy;
 
   beforeEach(async () => {
+    httpSpy = jasmine.createSpyObj('HttpClient',['get','post']);
+    authSpy = jasmine.createSpyObj('AuthService',['restoreSession']);
+    authSpy.restoreSession.and.returnValue(of(user));
+    httpSpy.get.and.returnValue(of(user))
+    authService = new AuthService(httpSpy);
+    wsSpy = jasmine.createSpyObj('window',['SockJS']);
+    clientSpy = jasmine.createSpyObj('Stomp', ['over']);
+
+
     await TestBed.configureTestingModule({
-      declarations: [ ChatComponent ]
+      declarations: [ ChatComponent ],
+      providers: [
+        {provide: AuthService, useValue: authSpy},
+        {provide: HttpClient, useValue: httpSpy},
+        {provide: SockJS, useValue: wsSpy},
+        {provide: over, useValue: clientSpy},
+      ]
+
     })
     .compileComponents();
 
@@ -20,4 +50,6 @@ describe('ChatComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  
 });
---
+++ src/app/services/socket-service.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketServiceService {
+
+  constructor() { }
+
+  getSocket = () => {
+    const socket = new SockJS(environment.baseUrl + '/ws');
+    return Stomp.over(socket);
+  }
+
+  }
+}
